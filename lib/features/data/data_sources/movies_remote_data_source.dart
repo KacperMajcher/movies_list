effcb248c877e39076dc02208d54a26393fcbc13
kacpermajcher <kacpermajcher13@gmail.com>
2023-12-08 18:30:25 +0100

    Add Dio handling errors

--- lib/features/data/data_sources/movies_remote_data_source.dart
+++ lib/features/data/data_sources/movies_remote_data_source.dart
@@ -2,8 +2,12 @@ import 'package:dio/dio.dart';
 
 class MoviesRemoteDioDataSource {
   Future<Map<String, dynamic>?> getMoviesData() async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=bcb3e9eb9e85d0415e3c55f0d8339031');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=bcb3e9eb9e85d0415e3c55f0d8339031');
+      return response.data;
+    } on DioException catch (error) {
+      throw Exception(error.response?.data['error'] ?? 'Unknown error');
+    }
   }
 }
